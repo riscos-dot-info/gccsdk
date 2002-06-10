3f1db889722c60ba04166461aecbc77341addf06
admin <admin>
2002-06-10 12:08:01 +0000

    Mon Jun 10 13:08:23 2002  Stefan Bellon  <sbellon@sbellon.de>
    	* unix/dev.c (__fsopen): Support PipeFS.

--- release_2_95/gcc/unixlib/source/unix/dev.c
+++ release_2_95/gcc/unixlib/source/unix/dev.c
@@ -1,15 +1,15 @@
 /****************************************************************************
  *
  * $Source: /usr/local/cvsroot/gccsdk/unixlib/source/unix/dev.c,v $
- * $Date: 2002/01/12 16:15:31 $
- * $Revision: 1.2.2.4 $
+ * $Date: 2002/06/10 12:01:27 $
+ * $Revision: 1.2.2.5 $
  * $State: Exp $
  * $Author: admin $
  *
  ***************************************************************************/
 
 #ifdef EMBED_RCSID
-static const char rcs_id[] = "$Id: dev.c,v 1.2.2.4 2002/01/12 16:15:31 admin Exp $";
+static const char rcs_id[] = "$Id: dev.c,v 1.2.2.5 2002/06/10 12:01:27 admin Exp $";
 #endif
 
 /* #define DEBUG */
@@ -164,9 +164,51 @@ __fsopen (struct __unixlib_fd *file_desc, const char *filename, int mode)
     }
   else
     {
-      /* If no file exists and O_CREAT was not specified, return ENOENT.  */
-      if (!(fflag & O_CREAT))
-	return (void *) __set_errno (ENOENT);
+      /* We try to do our best to get at the filing system name
+         by canonicalising the filename if possible, otherwise checking
+         for pipe: anyway.
+         NOTE: This is an ugly hack to make opening non-existent files
+	 for reading on PipeFS work.  We should handle PipeFS though
+         a proper device ioctl.  */
+      char *temp;
+      regs[0] = 37;
+      regs[1] = (int) filename;
+      regs[2] = 0;
+      regs[3] = 0;
+      regs[4] = 0;
+      regs[5] = 0;
+      __os_swi (OS_FSControl, regs);
+      temp = malloc (1 - regs[5]);
+      if (temp)
+	{
+	  regs[0] = 37;
+	  regs[1] = (int) filename;
+	  regs[2] = (int) temp;
+	  regs[3] = 0;
+	  regs[4] = 0;
+	  regs[5] = 1 - regs[5];
+	  __os_swi (OS_FSControl, regs);
+	} else
+	  temp = (char *) filename;
+      
+      if (! (strlen (temp) > 5
+	     && tolower (temp[0]) == 'p'
+	     && tolower (temp[1]) == 'i'
+	     && tolower (temp[2]) == 'p'
+	     && tolower (temp[3]) == 'e'
+	     && temp[4] == ':'))
+        {
+          /* If no file exists and O_CREAT was not specified,
+	     return ENOENT.  */
+          if (!(fflag & O_CREAT))
+	    {
+	      if (temp)
+		free(temp);
+	      return (void *) __set_errno (ENOENT);
+	    }
+        }
+      if (temp)
+        free(temp);
     }
 
   if (fflag & O_CREAT)
