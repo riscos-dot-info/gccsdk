2b625946b3192fc493db112128c75accc43de073
joty <joty>
2010-07-29 20:31:08 +0000

    License wise we need to go for GPL v3 or later.

--- recipe/files/gcc/gcc/config/arm/riscos-elf.h
+++ recipe/files/gcc/gcc/config/arm/riscos-elf.h
@@ -1,24 +1,23 @@
 /* Definitions for ARM running RISC OS using ELF
-   Copyright (C) 1993, 1994, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-   2005 Free Software Foundation, Inc.
-   Contributed by Nick Burrett <nick@sqrt.co.uk>
+   Copyright (C) 2005-2010 Free Software Foundation, Inc.
+   Contributed by Nick Burrett (nick@sqrt.co.uk>),
+   John Tytgat (John.Tytgat@aaug.net) and Lee Noar (leenoar@sky.com).
 
-   This file is part of GCC.
+This file is part of GCC.
 
-   GCC is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published
-   by the Free Software Foundation; either version 2, or (at your
-   option) any later version.
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
 
-   GCC is distributed in the hope that it will be useful, but WITHOUT
-   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-   License for more details.
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; see the file COPYING.  If not, write to
-   the Free Software Foundation, 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
 
 /* elfos.h should have already been included.  Now just override
    any conflicting definitions and add any extras.  */
--- recipe/files/gcc/gcc/config/arm/riscos.md
+++ recipe/files/gcc/gcc/config/arm/riscos.md
@@ -1,4 +1,23 @@
 ;; Implementations for dyamic allocation off the stack.
+;; Copyright (C) 2005-2010 Free Software Foundation, Inc.
+;; Contributed by Nick Burrett (nick@sqrt.co.uk>)
+;; and Lee Noar (leenoar@sky.com).
+
+;; This file is part of GCC.
+
+;; GCC is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+
+;; GCC is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GCC; see the file COPYING3.  If not see
+;; <http://www.gnu.org/licenses/>.
 
 ;; These patterns say how to perform an equivalent to dynamic allocation
 ;; off the stack. We use library routines to return malloced memory
