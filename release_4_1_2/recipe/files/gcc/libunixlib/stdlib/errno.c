59e2c4606983de7c32cd3aa45337987adf35ef15
joty <joty>
2012-04-21 15:02:24 +0000

    tags/release_4_1_2_v1: Delete tag before retag.

--- release_4_1_2/recipe/files/gcc/libunixlib/stdlib/errno.c
+++
@@ -1,9 +0,0 @@
-/* Definition of the symbol 'errno'.
-   Copyright (c) 2005-2010 UnixLib Developers.  */
-
-#include <errno.h>
-
-#if !__UNIXLIB_ERRNO_THREADED
-int errno;
-#endif
-
--- release_4_1_2/riscos/asasm/regression/tests_pass/area.s
+++
@@ -1,7 +0,0 @@
-	; Don't complain about AREA ABS difference.
-	AREA Code, CODE
-	ORG &100
-	AREA Code, CODE
-
-	END
-
